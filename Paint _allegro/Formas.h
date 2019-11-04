ab92441a2a7520da1468eadd51a60ab6a244a468
ertsaz <ertsaz9009@gmail.com>
2019-11-03 22:49:32 -0500

    subida de el proyecto a github

---
+++ Paint _allegro/Formas.h
@@ -0,0 +1,258 @@
+﻿#pragma once
+# include <allegro5\allegro.h>
+# include <allegro5\allegro_image.h>
+# include <allegro5\allegro_native_dialog.h>
+# include <allegro5\allegro_font.h>
+# include <allegro5\allegro_ttf.h>
+#include <allegro5\allegro_primitives.h>
+#include <allegro5/allegro_opengl.h>
+#include <math.h>
+#include <stdio.h>
+
+enum eforma
+{
+	NADA, LINEA_DDA, LINEA_B, RECTANGULO, CIRCULO, ELIPSE, TRIANGULO
+};
+
+#define BLANCO al_map_rgb(255, 255, 255)
+#define NEGRO al_map_rgb(0, 0, 0)
+#define DARK_BLUE al_map_rgb(0, 0, 139)
+#define MODER_ORANGE al_map_rgb(179, 106, 48)
+
+
+void Pixel(int32_t x, int32_t y, const ALLEGRO_COLOR col);
+
+void lineaDDA(int32_t x0, int32_t y0, int32_t x1, int32_t y1, const ALLEGRO_COLOR col);
+
+void linea(int32_t x0, int32_t y0, int32_t x1, int32_t y1, const ALLEGRO_COLOR col = BLANCO, uint32_t pattern = 0xFFFFFFFF);
+
+void rectangulo(int32_t x, int32_t y, int32_t w, int32_t h, const ALLEGRO_COLOR col);
+
+void circulo(int32_t r, int32_t xc, int32_t yc, const ALLEGRO_COLOR col);
+
+void elipse(int32_t rx, int32_t xc, int32_t yc, const ALLEGRO_COLOR col);
+
+void trgiso(int32_t x0, int32_t y0, int32_t x1, int32_t y1, const ALLEGRO_COLOR col);
+
+void Pixel(int32_t x, int32_t y, const ALLEGRO_COLOR col) {
+	al_put_pixel(x, y, col);
+}
+
+void linea(int32_t x0, int32_t y0, int32_t x1, int32_t y1, const ALLEGRO_COLOR col, uint32_t pattern) {
+	int x, y, dx, dy, p, sigX, sigY;
+
+	auto rol = [&](void)
+	{
+		pattern = (pattern << 1) | (pattern >> 31);
+		return pattern & 1;
+	};
+
+	dx = (x1 - x0);
+	dy = (y1 - y0);
+	/* determinar que punto usar para empezar, cual para terminar */
+	if (dy < 0) {
+		dy = -dy;
+		sigY = -1;
+	}
+	else {
+		sigY = 1;
+	}
+
+
+	if (dx < 0) {
+		dx = -dx;
+		sigX = -1;
+	}
+	else {
+		sigX = 1;
+	}
+
+
+	x = x0;
+	y = y0;
+
+	/* se cicla hasta llegar al extremo de la linea */
+	if (dx > dy) {
+		p = 2 * dy - dx;;
+		while (x != x1) {
+			x = x + sigX;
+			if (p < 0) {
+				p = p + (2 * dy);
+			}
+			else {
+				y = y + sigY;
+				p = p + (2 * (dy - dx));
+			}
+			if (rol()) Pixel(x, y, col);
+
+		}
+	}
+	else {
+		p = 2 * dx - dy;
+		while (y != y1) {
+			y = y + sigY;
+			if (p < 0) {
+				p = p + 2 * dx;
+			}
+			else {
+				x = x + sigX;
+				p = p + 2 * (dx - dy);
+			}
+			if (rol()) Pixel(x, y, col);
+		}
+	}
+}
+
+void rectangulo(int32_t x, int32_t y, int32_t w, int32_t h, const ALLEGRO_COLOR col) {
+
+	linea(x, y, x + w, y, col);
+	linea(x + w, y, x + w, y + h, col);
+	linea(x + w, y + h, x, y + h, col);
+	linea(x, y + h, x, y, col);
+}
+
+void circulo(int32_t r, int32_t xc, int32_t yc, const ALLEGRO_COLOR col) {
+
+	int x, y, rx2, ry2, p1, p2;
+
+	x = 0;
+	y = r;
+	rx2 = pow(r, 2);
+	ry2 = pow(r, 2);
+
+	p1 = ry2 - (rx2 * r) + (0.25 * rx2);
+
+	while ((ry2 * x) < (rx2 * y))
+	{
+		if (p1 < 0)
+		{
+			x++;
+			p1 = p1 + (2 * ry2 * x) + ry2;
+		}
+		else
+		{
+			x++; y--;
+			p1 = p1 + (2 * ry2 * x) - (2 * rx2 * y) + ry2;
+		}
+		Pixel(xc + x, yc + y, col);
+		Pixel(xc - x, yc + y, col);
+		Pixel(xc + x, yc - y, col);
+		Pixel(xc - x, yc - y, col);
+	}
+
+	p2 = (ry2)*pow((x + 0.5), 2) + (rx2)*pow((y - 1), 2) - (rx2 * ry2);
+
+	while (y > 0)
+	{
+		if (p2 > 0)
+		{
+			y--;
+			p2 = p2 - (2 * rx2 * y) + rx2;
+		}
+		else
+		{
+			x++; y--;
+			p2 = p2 + (2 * ry2 * x) - (2 * rx2 * y) + rx2;
+		}
+		Pixel(xc + x, yc + y, col);
+		Pixel(xc - x, yc + y, col);
+		Pixel(xc + x, yc - y, col);
+		Pixel(xc - x, yc - y, col);
+	}
+}
+
+void elipse(int32_t rx, int32_t xc, int32_t yc, const ALLEGRO_COLOR col) {
+
+	int x, y, rx2, ry2, p1, p2;
+	int ry;
+
+	x = 0;
+	ry = rx / 2;
+	y = ry;
+	rx2 = pow(rx, 2);
+	ry2 = pow(ry, 2);
+
+	p1 = ry2 - (rx2 * ry) + (0.25 * rx2);
+
+	while ((ry2 * x) < (rx2 * y))
+	{
+		if (p1 < 0)
+		{
+			x++;
+			p1 = p1 + (2 * ry2 * x) + ry2;
+		}
+		else
+		{
+			x++; y--;
+			p1 = p1 + (2 * ry2 * x) - (2 * rx2 * y) + ry2;
+		}
+		Pixel(xc + x, yc + y, col);
+		Pixel(xc - x, yc + y, col);
+		Pixel(xc + x, yc - y, col);
+		Pixel(xc - x, yc - y, col);
+	}
+
+	p2 = (ry2)*pow((x + 0.5), 2) + (rx2)*pow((y - 1), 2) - (rx2 * ry2);
+
+	while (y > 0)
+	{
+		if (p2 > 0)
+		{
+			y--;
+			p2 = p2 - (2 * rx2 * y) + rx2;
+		}
+		else
+		{
+			x++; y--;
+			p2 = p2 + (2 * ry2 * x) - (2 * rx2 * y) + rx2;
+		}
+		Pixel(xc + x, yc + y, col);
+		Pixel(xc - x, yc + y, col);
+		Pixel(xc + x, yc - y, col);
+		Pixel(xc - x, yc - y, col);
+	}
+}
+
+void trgiso(int32_t x0, int32_t y0, int32_t x1, int32_t y1, const ALLEGRO_COLOR col) {
+
+	int r;
+	r = sqrt((pow((x1 - x0), 2)) + (pow((y1 - y0), 2))); // CAMBIALO
+	linea(x0, y0, x1, y1, col);
+	linea(x1, y1, x0 + r, y0, col);
+	linea(x0, y0, x0 + r, y0, col);
+
+}
+
+void lineaDDA(int32_t x0, int32_t y0, int32_t x1, int32_t y1, const ALLEGRO_COLOR col) {
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+
+	if (abs(dx) > abs(dy)) {          // pendiente < 1
+		float m = (float)dy / (float)dx;
+		float b = y0 - m * x0;
+		if (dx < 0)
+			dx = -1;
+		else
+			dx = 1;
+		while (x0 != x1) {
+			x0 += dx;
+			y0 = round(m * x0 + b); //calcula con la ecuacion de la recta
+			Pixel(x0, y0, col);
+		}
+	}
+	else
+		if (dy != 0) {                              // pendiente >= 1
+			float m = (float)dx / (float)dy;
+			float b = x0 - m * y0;
+			if (dy < 0)
+				dy = -1;
+			else
+				dy = 1;
+			while (y0 != y1) {
+				y0 += dy;
+				x0 = round(m * y0 + b);
+				Pixel(x0, y0, col);//calcula con la ecuacion de la recta
+			}
+		}
+}
+
---
+++ Paint _allegro/Primitivas.cpp
@@ -0,0 +1,562 @@
+#include "Formas.h"
+#include <ctype.h>
+#include <vector>
+#include <list>
+#define ALLEGRO_UNSTABLE
+#define RADIUS       5
+
+struct Example {
+	ALLEGRO_DISPLAY* sVentana;
+	ALLEGRO_FONT* fuente;
+	ALLEGRO_FONT* fuentebmp;
+	ALLEGRO_EVENT_QUEUE* colaevento;
+	ALLEGRO_BITMAP* dbuf;
+	ALLEGRO_COLOR     bg;
+	ALLEGRO_COLOR     fg;
+};
+
+static struct Example ini;
+
+struct vf2d
+{
+	float x;
+	float y;
+};
+
+vf2d vOffset = { 0.0f, 0.0f };
+vf2d vStartPan = { 0.0f, 0.0f };
+float fScale = 10.0f;
+
+// Convert coordinates from World Space --> Screen Space
+void WorldToScreen(const float v_x, const float v_y, int& nScreenX, int& nScreenY)
+{
+	nScreenX = (int)((v_x - vOffset.x) * fScale);
+	nScreenY = (int)((v_y - vOffset.y) * fScale);
+}
+
+// Convert coordinates from Screen Space --> World Space
+void ScreenToWorld(int nScreenX, int nScreenY, vf2d& v)
+{
+	v.x = (float)(nScreenX) / fScale + vOffset.x;
+	v.y = (float)(nScreenY) / fScale + vOffset.y;
+}
+
+void destroy() {
+	if (ini.sVentana)
+		al_destroy_display(ini.sVentana);
+	if (ini.fuente)
+		al_destroy_font(ini.fuente);
+	if (ini.colaevento)
+		al_destroy_event_queue(ini.colaevento);
+
+}
+
+int ini_allegro(int nAnc, int nAlt, const char* tTitulo) {
+	if (!al_init()) {
+		al_show_native_message_box(NULL, "ERRO",
+			"Se produjo el siguiente error y el programa finalizará:",
+			"Error al inicializar allegro", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return 0;
+	}
+
+
+	ini.sVentana = al_create_display(nAnc, nAlt);
+	if (!ini.sVentana) {
+		al_show_native_message_box(NULL, "ERRO",
+			"Se produjo el siguiente error y el programa finalizará:",
+			"Error al crear Ventana", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		destroy();
+		return 0;
+	}
+	al_set_window_title(ini.sVentana, tTitulo);
+
+	al_init_primitives_addon();
+
+	if (!al_init_image_addon()) {
+		al_show_native_message_box(NULL, "ERRO",
+			"Se produjo el siguiente error y el programa finalizará:",
+			"Error al inicializar el complemento de imagen", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		destroy();
+		return 0;
+	}
+
+	/*fundo = al_load_bitmap("img/icons8.png");
+	if (!fundo) {
+
+		destroy();
+		return 0;
+	}*/
+
+	/*flecha_titulo = al_load_bitmap("img/arrow.png");
+	if (!flecha_titulo) {
+		al_show_native_message_box(NULL, "ERRO",
+			"Se produjo el siguiente error y el programa finalizará:",
+			"La imagen no se pudo cargar", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		destroy();
+		return 0;
+	}*/
+
+	al_init_font_addon();
+	if (!al_init_ttf_addon()) {
+		al_show_native_message_box(NULL, "ERRO",
+			"Se produjo el siguiente error y el programa finalizará:",
+			"Error al inicializar el complemento allegro_ttf", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		destroy();
+		return 0;
+	}
+
+	/*vecPuntos = al_load_font("manaspc.ttf", 32, 0);
+	titulo = al_load_font("manaspc.ttf", 56, 0);
+	if (!vecPuntos || !titulo) {
+		al_show_native_message_box(NULL, "ERRO",
+			"Se produjo el siguiente error y el programa finalizará:",
+			"Error al cargar la vecPuntos", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		destroy();
+		return 0;
+	}*/
+
+	if (!al_install_keyboard()) {
+		al_show_native_message_box(NULL, "ERRO",
+			"Se produjo el siguiente error y el programa finalizará:",
+			"Error al inicializar el teclado", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		destroy();
+		return 0;
+	}
+
+	if (!al_install_mouse()) {
+		al_show_native_message_box(NULL, "ERRO",
+			"Se produjo el siguiente error y el programa finalizará:",
+			"Error al inicializar el mouse", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		destroy();
+		return 0;
+	}
+
+	ini.colaevento = al_create_event_queue();
+	if (!ini.colaevento) {
+		al_show_native_message_box(NULL, "ERRO",
+			"Se produjo el siguiente error y el programa finalizará:",
+			"Error al crear la cola de eventose", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		destroy();
+		return 0;
+	}
+
+	/*timer = al_create_timer(1.0 / FPS);
+	if (!timer) {
+		al_show_native_message_box(NULL, "ERRO",
+			"Se produjo el siguiente error y el programa finalizará:",
+			"Error al crear el temporizador", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		destroy();
+		return 0;
+	}*/
+
+	al_register_event_source(ini.colaevento, al_get_display_event_source(ini.sVentana));
+	al_register_event_source(ini.colaevento, al_get_mouse_event_source());
+	al_register_event_source(ini.colaevento, al_get_keyboard_event_source());
+	/*al_register_event_source(fila_eventos, al_get_timer_event_source(timer));
+	al_start_timer(timer);*/
+
+	return 1;
+}
+
+struct sFormas;
+
+// Define a node
+struct sPuntos
+{
+	sFormas* tipo_forma;
+	vf2d pos;
+};
+
+
+// Clase BASE define la interfaz para todas las formas.
+struct sFormas
+{
+	// Shapes are defined by the placment of nodes
+	std::vector<sPuntos> vecPuntos;
+	int nMaxPunto = 0;
+
+	// The colour of the shape
+	ALLEGRO_COLOR col = al_map_rgb(0, 128, 0);
+
+	// All shapes share word to screen transformation
+	// coefficients, so share them staically
+	static float fWorldScale;
+	static vf2d vWorldOffset;
+
+	// Convert coordinates from World Space --> Screen Space
+	void MundoAScreen(const vf2d& v, int& nScreenX, int& nScreenY)
+	{
+		nScreenX = (int)((v.x - vWorldOffset.x) * fWorldScale);
+		nScreenY = (int)((v.y - vWorldOffset.y) * fWorldScale);
+	}
+
+	// This is a PURE function, which makes this class abstract. A sub-class
+	// of this class must provide an implementation of this function by
+	// overriding it
+	virtual void DibujarForma() = 0;
+
+	// Shapes are defined by nodes, the shape is responsible
+	// for issuing nodes that get placed by the user. The shape may
+	// change depending on how many nodes have been placed. Once the
+	// maximum number of nodes for a shape have been placed, it returns
+	// nullptr
+	sPuntos* OptenerPunto(const vf2d& p)
+	{
+		if (vecPuntos.size() == nMaxPunto)
+			return nullptr; // Shape is complete so no new nodes to be issued
+
+		// else create new node and add to shapes node vector
+		sPuntos n;
+		n.tipo_forma = this;
+		n.pos = p;
+		vecPuntos.push_back(n);
+
+		// Beware! - This normally is bad! But see sub classes
+		return &vecPuntos[vecPuntos.size() - 1];
+	}
+
+	// Test to see if supplied coordinate exists at same location
+	// as any of the nodes for this shape. Return a pointer to that
+	// node if it does
+	sPuntos* HitPunto(vf2d& p)
+	{
+		for (auto& n : vecPuntos)
+		{
+			float dx = n.pos.x - p.x;
+			float dy = n.pos.y - p.y;
+			float dd = dx * dx + dy * dy;
+			if (dd < 0.01f)
+				return &n;
+		}
+
+		return nullptr;
+	}
+
+	// Draw all of the nodes that define this shape so far
+	void DibujarPuntos()
+	{
+		int i = 0;
+		for (auto& n : vecPuntos)
+		{
+			i++;
+			int sx, sy;
+			MundoAScreen(n.pos, sx, sy);
+			circulo(RADIUS, sx, sy, MODER_ORANGE);
+		}
+	}
+};
+
+// We must provide an implementation of our static variables
+float sFormas::fWorldScale = 1.0f;
+vf2d sFormas::vWorldOffset = { 0,0 };
+
+// LINEA sub clase, hereda desde sFormas
+struct sLinea : public sFormas
+{
+	sLinea()
+	{
+		nMaxPunto = 2;
+		vecPuntos.reserve(nMaxPunto);
+
+	}
+
+	void DibujarForma()
+	{
+		int sx, sy, ex, ey;
+		MundoAScreen(vecPuntos[0].pos, sx, sy);
+		MundoAScreen(vecPuntos[1].pos, ex, ey);
+		linea(sx, sy, ex, ey, col, 0xFFFFFFFF);
+	}
+};
+
+// RECTANGULO
+struct sRec : public sFormas
+{
+	sRec()
+	{
+		nMaxPunto = 2;
+		vecPuntos.reserve(nMaxPunto);
+	}
+
+	void DibujarForma() 
+	{
+		int sx, sy, ex, ey;
+		MundoAScreen(vecPuntos[0].pos, sx, sy);
+		MundoAScreen(vecPuntos[1].pos, ex, ey);
+		rectangulo(sx, sy, ex - sx, ey - sy, col);
+	}
+};
+
+// CIRCULOS
+struct sCir : public sFormas
+{
+	sCir()
+	{
+		nMaxPunto = 2;
+		vecPuntos.reserve(nMaxPunto);
+	}
+
+	void DibujarForma()
+	{
+
+		float dx = (vecPuntos[0].pos.x - vecPuntos[1].pos.x);
+		float dy = (vecPuntos[0].pos.y - vecPuntos[1].pos.y);
+		float fRadius = (dx * dx - dy * dy);
+		int sx, sy, ini, ey;
+		MundoAScreen(vecPuntos[0].pos, sx, sy);
+		MundoAScreen(vecPuntos[1].pos, ini, ey);
+		linea(sx, sy, ini, ey, col, 0xFF00FF00);
+
+		// Note the radius is also scaled so it is drawn appropriately
+		circulo(sx, sy, (int32_t)(fRadius * fWorldScale), col);
+	}
+};
+
+// TRIANGULO
+struct sTriangulo : public sFormas
+{
+	sTriangulo()
+	{
+		nMaxPunto = 3;
+		vecPuntos.reserve(nMaxPunto);
+	}
+
+	void DibujarForma()
+	{
+		int sx, sy, ini, ey;
+
+		if (vecPuntos.size() < 3)
+		{
+			// Can only draw line from first to second
+			MundoAScreen(vecPuntos[0].pos, sx, sy);
+			MundoAScreen(vecPuntos[1].pos, ini, ey);
+			linea(sx, sy, ini, ey, col, 0xFF00FF00);
+		}
+
+		if (vecPuntos.size() == 3)
+		{
+			// dibuja línea de primera punto a segunda punto
+			MundoAScreen(vecPuntos[0].pos, sx, sy);
+			MundoAScreen(vecPuntos[1].pos, ini, ey);
+			linea(sx, sy, ini, ey, col, 0xFFFFFFFF);
+
+			// dibuja línea de segunda punto a tercer punto
+			MundoAScreen(vecPuntos[1].pos, sx, sy);
+			MundoAScreen(vecPuntos[2].pos, ini, ey);
+			linea(sx, sy, ini, ey, col, 0xFFFFFFFF);
+
+			// dibuja línea de tercer punto a primer punto
+			MundoAScreen(vecPuntos[2].pos, sx, sy);
+			MundoAScreen(vecPuntos[0].pos, ini, ey);
+			linea(sx, sy, ini, ey, col, 0xFFFFFFFF);
+		}
+
+	}
+};
+
+int main(int argc, char** argv)
+{
+	ALLEGRO_EVENT event;
+	std::list<sFormas*> listShapes;
+	sFormas* formaTemp = nullptr;
+	sPuntos* selecPunto = nullptr;
+	vf2d vMouse;
+	float fGrid = 1.0f;
+	bool mdown = false;
+	int nForma = NADA;
+
+	(void)argc;
+	(void)argv;
+
+	if (!ini_allegro(1400, 800, "Herramientas de Programacion Grafica"))
+		return -1;
+
+	vOffset = { (float)(-al_get_display_width(ini.sVentana) / 2) / fScale, (float)(-al_get_display_height(ini.sVentana) / 2) / fScale };
+
+	while (true) {
+
+		/*************************************************acualizacion de pantalla*******************************************************/
+		al_wait_for_event(ini.colaevento, &event);
+		vMouse = { (float)event.mouse.x ,(float)event.mouse.y };
+		if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+			break;
+		if (event.type == ALLEGRO_EVENT_KEY_CHAR) {
+			if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+				break;
+			switch (toupper(event.keyboard.unichar)) {
+			case ' ':
+
+				break;
+			case 'L':
+
+				formaTemp = new sLinea();
+
+				// Place first node at location of keypress
+				selecPunto = formaTemp->OptenerPunto(vMouse);
+
+				// Get Second node
+				selecPunto = formaTemp->OptenerPunto(vMouse);
+				
+				/*nForma = LINEA_B;
+				if (nForma == LINEA_B)
+				{
+					nForma = NADA;
+				}*/
+				break;
+			case 'C':
+				nForma = CIRCULO;
+				break;
+			case 'R':
+				nForma = RECTANGULO;
+				break;
+			case 'E':
+				nForma = ELIPSE;
+				break;
+			case 'T':
+				nForma = TRIANGULO;
+				break;
+			}
+		}
+		if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+			if (event.mouse.button == 1)
+			{
+				if (formaTemp != nullptr)
+				{
+					selecPunto = formaTemp->OptenerPunto(vMouse);
+					if (selecPunto == nullptr)
+					{
+						formaTemp->col = BLANCO;
+						listShapes.push_back(formaTemp);
+					}
+				}
+				if (selecPunto != nullptr)
+				{
+					selecPunto->pos = vMouse;
+				}
+				//if (nForma == LINEA_B)
+				//{
+				//	formaTemp = new sLinea();
+
+				//	// Place first node at location of keypress
+				//	selecPunto = formaTemp->OptenerPunto(vMouse);
+
+				//	// Get Second node
+				//	selecPunto = formaTemp->OptenerPunto(vMouse);
+				//	nForma = NADA;
+				//}
+				//else if (nForma == CIRCULO)
+				//{
+				//	formaTemp = new sCir();
+
+				//	// Place first node at location of keypress
+				//	selecPunto = formaTemp->OptenerPunto(vMouse);
+
+				//	// Get Second node
+				//	selecPunto = formaTemp->OptenerPunto(vMouse);
+				//	nForma = NADA;
+				//}
+				//else if (nForma == RECTANGULO)
+				//{
+				//	formaTemp = new sRec();
+
+				//	// Place first node at location of keypress
+				//	selecPunto = formaTemp->OptenerPunto(vMouse);
+
+				//	// Get Second node
+				//	selecPunto = formaTemp->OptenerPunto(vMouse);
+				//	nForma = NADA;
+				//}
+			}
+			else if (event.mouse.button == 2) 
+			{
+				
+			}
+			//rclick(x, y);
+			else if (event.mouse.button == 3)
+			{
+				vStartPan.x = vMouse.x;
+				vStartPan.y = vMouse.y;
+				mdown = true;
+			}
+		}
+		if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
+
+			if (event.mouse.button == 2)
+			{
+
+			}
+			/*if (formaTemp != nullptr)
+			{
+				selecPunto = formaTemp->OptenerPunto(vMouse);
+				if (selecPunto == nullptr)
+				{
+					formaTemp->col = BLANCO;
+					listShapes.push_back(formaTemp);
+				}
+			}*/
+			mdown = false;
+		}
+		if (event.type == ALLEGRO_EVENT_MOUSE_AXES) {
+			if (mdown)
+			{
+				vOffset.x -= (vMouse.x - vStartPan.x) / fScale;
+				vOffset.y -= (vMouse.y - vStartPan.y) / fScale;
+				vStartPan = vMouse;
+			}
+			else if (selecPunto != nullptr)
+			{
+				selecPunto->pos = vMouse;
+			}
+		}
+
+		/*************************************************acualizacion de pantalla*******************************************************/
+		if (al_is_event_queue_empty(ini.colaevento)) {
+
+			al_clear_to_color(al_map_rgb(14, 14, 15));
+
+			int sx, sy;
+			int ex, ey;
+
+			// Get visible world
+			vf2d vWorldTopLeft, vWorldBottomRight;
+			ScreenToWorld(0, 0, vWorldTopLeft);
+			ScreenToWorld(al_get_display_width(ini.sVentana), al_get_display_height(ini.sVentana), vWorldBottomRight);
+
+			// Get values just beyond screen boundaries
+			vWorldTopLeft.x = floor(vWorldTopLeft.x);
+			vWorldTopLeft.y = floor(vWorldTopLeft.y);
+			vWorldBottomRight.x = ceil(vWorldBottomRight.x);
+			vWorldBottomRight.y = ceil(vWorldBottomRight.y);
+
+			WorldToScreen(0, vWorldTopLeft.y, sx, sy);
+			WorldToScreen(0, vWorldBottomRight.y, ex, ey);
+			linea(sx, sy, ex, ey, al_map_rgb(36, 64, 45), 0xFF00FF00);
+			WorldToScreen(vWorldTopLeft.x, 0, sx, sy);
+			WorldToScreen(vWorldBottomRight.x, 0, ex, ey);
+			linea(sx, sy, ex, ey, al_map_rgb(36, 64, 45), 0xFF00FF00);
+
+			// Update shape translation coefficients
+			sFormas::fWorldScale = fScale;
+			sFormas::vWorldOffset.x = vOffset.x;
+			sFormas::vWorldOffset.y = vOffset.y;
+
+			for (auto& forma : listShapes)
+			{
+				forma->DibujarForma();
+				forma->DibujarPuntos();
+			}
+
+			if (formaTemp != nullptr)
+			{
+				formaTemp->DibujarForma();
+				formaTemp->DibujarPuntos();
+			}
+			al_set_target_backbuffer(ini.sVentana);
+			al_flip_display();
+		}
+	}
+
+	destroy();
+	return 0;
+}
+
